6d0e608f51e84aa67eb85dbbda0d4541483af3d8
Vova112 <vtorkonak@gmail.com>
2021-02-24 20:13:34 +0200

    Lesson 19 Routing realization for Dialogs and Profiles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 
-const App = () => {
-  return (< div className="app-wrapper" >
-    <Header />
-    <Navbar />
-    <div className="app-wrapper-content">
-      <Dialogs />
-    </div>
-    { /* <Profile /> */}
-  </div>
+const App = (props) => {
+  return (
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route path="/dialogs" component={Dialogs} />
+          <Route path="/profile" component={Profile} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
