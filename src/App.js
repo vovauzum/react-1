7f96f2728f80141fa9ec7db6a28f29303bad14a3
Vova112 <vtorkonak@gmail.com>
2021-02-24 20:46:25 +0200

    Homework Routing realization for News Music and Settings

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = (props) => {
   return (
@@ -15,6 +18,9 @@ const App = (props) => {
         <div className="app-wrapper-content">
           <Route path="/dialogs" component={Dialogs} />
           <Route path="/profile" component={Profile} />
+          <Route path="/news" component={News} />
+          <Route path="/music" component={Music} />
+          <Route path="/settings" component={Settings} />
         </div>
       </div>
     </BrowserRouter>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,16 +8,16 @@ const Navbar = () => {
         <a href="/profile">Profile</a>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a href="/dialogs">Messages</a>
+        <a href="/dialogs">Dialogs</a>
       </div>
       <div className={s.item}>
-        <a>News</a>
+        <a href="/news">News</a>
       </div>
       <div className={s.item}>
-        <a>Music</a>
+        <a href="/music">Music</a>
       </div>
       <div className={s.item}>
-        <a>Settings</a>
+        <a href="/settings">Settings</a>
       </div>
     </nav>
   )
