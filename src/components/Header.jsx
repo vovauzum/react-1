4db93a9c13101fd17c1bd1619323af0167c1b0d8
Vova112 <vtorkonak@gmail.com>
2021-02-24 14:57:47 +0200

    added css files for js files in component directory

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
     return (
         <header className="header">
-            <img src="https://www.vhv.rs/dpng/d/483-4839905_logo-no-background-grey-large-transparent-background-cool.png" alt="" />
+            <img className="header__img" src="https://www.vhv.rs/dpng/d/483-4839905_logo-no-background-grey-large-transparent-background-cool.png" alt="" />
         </header>
     )
 }
