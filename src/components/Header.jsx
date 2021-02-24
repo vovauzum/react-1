f85b708945aad5fd48e20b5313bf165373892320
Vova112 <vtorkonak@gmail.com>
2021-02-24 13:55:15 +0200

    lesson 12 Added header navbar profile

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://www.vhv.rs/dpng/d/483-4839905_logo-no-background-grey-large-transparent-background-cool.png" alt="" />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://wallpaperaccess.com/full/1940067.jpg" alt="" />
+      </div>
+      <div>
+        ava + description
+        </div>
+      <div>
+        My posts
+          <div>
+          New post
+          </div>
+        <div>
+          post 1
+          </div>
+        <div>
+          post 2
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
