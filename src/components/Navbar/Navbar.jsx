bd5fcc85fcf2bd7135497762aa5f4c00386d3bd7
Vova112 <vtorkonak@gmail.com>
2021-02-25 15:10:31 +0200

    Lesson 20 Navbar tag a to Navlink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <a href="/profile">Profile</a>
+        <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
       </div>
       <div className={`${s.item} ${s.active}`}>
-        <a href="/dialogs">Dialogs</a>
+        <NavLink to="/dialogs" activeClassName={s.activeLink}>Dialogs</NavLink>
       </div>
       <div className={s.item}>
-        <a href="/news">News</a>
+        <NavLink to="/news" activeClassName={s.activeLink}>News</NavLink>
       </div>
       <div className={s.item}>
-        <a href="/music">Music</a>
+        <NavLink to="/music" activeClassName={s.activeLink}>Music</NavLink>
       </div>
       <div className={s.item}>
-        <a href="/settings">Settings</a>
+        <NavLink to="/settings" activeClassName={s.activeLink}>Settings</NavLink>
       </div>
     </nav>
   )
