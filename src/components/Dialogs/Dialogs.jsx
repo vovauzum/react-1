356db329b216b1cd7791f54968d069378a4cb694
Vova112 <vtorkonak@gmail.com>
2021-02-25 15:33:32 +0200

    Lesson 21 Added dialogs to Dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import s from "./Dialogs.module.css";
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + " " + s.active}>
+                    Eduard
+                </div>
+                <div className={s.dialog}>
+                    Igor
+                </div>
+                <div className={s.dialog}>
+                    Vova
+                </div>
+                <div className={s.dialog}>
+                    Olya
+                </div>
+                <div className={s.dialog}>
+                    Yulya
+                </div>
+                <div className={s.dialog}>
+                    Igor
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your it-kamasutra</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 }
