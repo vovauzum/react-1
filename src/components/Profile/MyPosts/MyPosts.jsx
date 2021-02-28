5e01bd8cff5f8d78b3174fcca3e47611e72fa1f9
Vova112 <vtorkonak@gmail.com>
2021-02-28 14:54:04 +0200

    Lesson 31 Adden onClick => alert textarea on buttons

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,17 +3,23 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
-
     let postsElements = props.posts.map(p => <Post message={p.message} likeCounts={p.likeCounts} />);
+
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+    }
 
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
-                <button>Add post</button>
+                <button onClick={ addPost }>Add post</button>
             </div>
             <div className={s.posts}>
                 {postsElements}
