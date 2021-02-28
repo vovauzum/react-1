406f4acd49338a82b130988ff8a11a36cb49979a
Vova112 <vtorkonak@gmail.com>
2021-02-28 14:54:04 +0200

    Lesson 31 Added onClick => alert textarea on buttons

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -8,6 +8,13 @@ const Dialogs = (props) => {
     let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
     let messagesElements = props.state.messages.map(m => <Message message={m.message} />);
 
+    let newMessageElement = React.createRef();
+
+
+    let addMessage = () => {
+        let text = newMessageElement.current.value;
+        alert(text);
+    }
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -15,6 +22,10 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+            </div>
+            <div>
+                <textarea ref={newMessageElement}></textarea>
+                <button onClick={ addMessage }>Add Message</button>
             </div>
         </div>
     )
