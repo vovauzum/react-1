a17ef1e453ce103f369b4a57698a8aba9883d63b
Vova112 <vtorkonak@gmail.com>
2021-02-28 13:29:06 +0200

    Lesson 29 Fixed*

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import state from './redux/state';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App appState={state} />
+        <App state={state} />
     </React.StrictMode>,
     document.getElementById('root')
 );
