1cf11e75d9e9cab84cf5393b0328cd41cb2fad9f
Vova112 <vtorkonak@gmail.com>
2021-03-01 15:18:15 +0200

    Lesson 38 Added dispatch method

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import reportWebVitals from './reportWebVitals';
-import store from './redux/state';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/state";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={store.addPost.bind(store)}
-            updateNewPostText={store.updateNewPostText.bind(store)} />
+            <App state={state} dispatch={store.dispatch.bind(store)} />
         </BrowserRouter>,
-        document.getElementById('root')
+        document.getElementById("root")
     );
 }
 
