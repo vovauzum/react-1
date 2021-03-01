9bf94408f1dd3528ef918befcd40932d54198552
Vova112 <vtorkonak@gmail.com>
2021-03-01 16:10:39 +0200

    Lesson 39 Added actionCreator

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+
 let store = {
     _state: {
         profilePage: {
@@ -37,9 +40,9 @@ let store = {
     subscribe(observer) {
         this._callSubscriber = observer;
     },
-    
+
     dispatch(action) {
-        if (action.type === "ADD-POST") {
+        if (action.type === ADD_POST) {
             let newPost = {
                 id: 5,
                 message: this._state.profilePage.newPostText,
@@ -49,13 +52,17 @@ let store = {
             this._state.profilePage.newPostText = "";
             this._callSubscriber(this._state);
         }
-        else if (action.type === "UPDATE-NEW-POST-TEXT") {
+        else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
         }
     }
+
 }
 
+export const addPostActionCreator = () => ({ type: "ADD-POST" })
+export const updateNewPostTextActionCreator = (text) =>
+    ({ type: "UPDATE-NEW-POST-TEXT", newText: text })
 
 export default store;
 window.store = store;
