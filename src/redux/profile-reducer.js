0017d66d38d653d4c68fd18d7902d79abdd2fa12
Vova112 <vtorkonak@gmail.com>
2021-03-04 21:08:21 +0200

    Lesson 47 stateCopy for dialogs-reduser

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -12,24 +12,21 @@ let initialState = {
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST:
-            {
-                let newPost = {
-                    id: 5,
-                    message: state.newPostText,
-                    likeCounts: 0
-                };
-                let stateCopy = {...state };
-                stateCopy.posts = [...state.posts];
-                stateCopy.posts.push(newPost);
-                stateCopy.newPostText = "";
-                return stateCopy;
-            }
+            let newPost = {
+                id: 5,
+                message: state.newPostText,
+                likeCounts: 0
+            };
+            return {
+                ...state,
+                posts: [...state.posts, newPost],
+                newPostText: ""
+            };
         case UPDATE_NEW_POST_TEXT:
-            {
-                let stateCopy = {...state };
-                stateCopy.newPostText = action.newText;
-                return stateCopy;
-            }
+            return {
+                ...state,
+                newPostText: action.newText
+            };
         default:
             return state;
     }
