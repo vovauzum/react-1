6b33c1b566f1de3f474c244c8e2ffcdb5c6759ca
Vova112 <vtorkonak@gmail.com>
2021-03-10 20:05:17 +0200

    Lesson 67 redux-thunk 2
    thunk for profile and header

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,24 +1,15 @@
-import axios from "axios";
 import React from "react";
 import { connect } from "react-redux";
 import Header from "./Header";
-import { setAuthUserData} from "../../redux/auth-reducer";
+import { getAuthUserData } from "../../redux/auth-reducer";
 
 class HeaderContainer extends React.Component {
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
-            withCredentials: true
-        })
-            .then(response => {
-                if (response.data.resultCode === 0) {
-                    let {id, email, login} = response.data.data;
-                    this.props.setAuthUserData(id, email, login);
-                }
-            });
+        this.props.getAuthUserData();
     }
-    
+
     render() {
-    return <Header {...this.props} />
+        return <Header {...this.props} />
     }
 }
 
@@ -26,4 +17,5 @@ const mapStateToProps = (state) => ({
     isAuth: state.auth.isAuth,
     login: state.auth.login
 });
-export default connect(mapStateToProps, {setAuthUserData})(HeaderContainer);
+
+export default connect(mapStateToProps, { getAuthUserData })(HeaderContainer);
