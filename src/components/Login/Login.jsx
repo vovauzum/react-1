0cb516d55af9ce7daa2926bb9545b59a984d6bed
Vova112 <vtorkonak@gmail.com>
2021-03-14 20:16:35 +0200

    Lesson 79 stopSubmit() redux-form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,46 +3,50 @@ import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
 import { required } from "../../utils/validators/validators";
 import { Input } from "../common/FormsControls/FormsControls";
-import {login} from "../../redux/auth-reducer";
+import { login } from "../../redux/auth-reducer";
 import { Redirect } from "react-router";
+import style from "../common/FormsControls/FormsControls.module.css";
 
 const LoginForm = (props) => {
     return <form onSubmit={props.handleSubmit}>
         <div>
-            <Field placeholder={"Email"} 
-            validate={[required]}
-            name={"email"} 
-            component={Input}/>
+            <Field placeholder={"Email"}
+                validate={[required]}
+                name={"email"}
+                component={Input} />
         </div>
         <div>
-            <Field 
-            validate={[required]}
-            placeholder={"Password"} 
-            name={"password"} 
-            component={Input}
-            type="password"/>
+            <Field
+                validate={[required]}
+                placeholder={"Password"}
+                name={"password"}
+                component={Input}
+                type="password" />
         </div>
         <div>
             <Field component={Input} type={"checkbox"} name={"rememberMe"} />Remember me
         </div>
+        {props.error && <div className={style.formSummaryError}>
+            {props.error}
+        </div>}
         <div>
             <button>Login</button>
         </div>
     </form>
 }
 
-const LoginReduxForm = reduxForm({form: "login"}) (LoginForm)
+const LoginReduxForm = reduxForm({ form: "login" })(LoginForm)
 
 const Login = (props) => {
     const onSubmit = (formData) => {
         props.login(formData.email, formData.password, formData.rememberMe);
     }
     if (props.isAuth) {
-        return <Redirect to={"/profile"}/>
+        return <Redirect to={"/profile"} />
     }
     return <div>
         <h1>LOGIN</h1>
-    <LoginReduxForm onSubmit={onSubmit}/>
+        <LoginReduxForm onSubmit={onSubmit} />
     </div>
 }
 
@@ -50,4 +54,4 @@ const mapStateToProps = (state) => ({
     isAuth: state.auth.isAuth
 })
 
-export default connect(mapStateToProps, {login})(Login);
+export default connect(mapStateToProps, { login })(Login);
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -11,21 +11,25 @@ class ProfileContainer extends React.Component {
     componentDidMount() {
         let userId = this.props.match.params.userId;
         if (!userId) {
-            userId = 15622;
+            userId = this.props.authorizedUserId;
         }
         this.props.getUserProfile(userId);
         this.props.getStatus(userId);
     }
     render() {
         return (
-            <Profile {...this.props} profile={this.props.profile}
-            status={this.props.status} updateStatus={this.props.updateStatus}/>
+            <Profile {...this.props} 
+            profile={this.props.profile}
+            status={this.props.status} 
+            updateStatus={this.props.updateStatus}/>
         )
     }
 }
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
-    status: state.profilePage.status
+    status: state.profilePage.status,
+    authorizedUserId: state.auth.userId,
+    isAuth: state.auth.isAuth
 });
 
 export default compose(
