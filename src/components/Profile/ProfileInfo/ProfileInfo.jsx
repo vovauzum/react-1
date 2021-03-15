ff468cd0a982ead4ce1d555b3731253c3994d5c5
Vova112 <vtorkonak@gmail.com>
2021-03-15 19:01:15 +0200

    Lesson 84 hook useState

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Preloader from "../../common/Preloader/Preloader";
 import s from "./ProfileInfo.module.css";
-import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -15,7 +15,7 @@ const ProfileInfo = (props) => {
             </div> */}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} />
-                <ProfileStatus status={props.status} updateStatus= {props.updateStatus}/>
+                <ProfileStatusWithHooks status={props.status} updateStatus= {props.updateStatus}/>
             </div>
         </div>
     )
