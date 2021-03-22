be6a3dbe396cbb054a2eb62bfc724f394993af86
Vova112 <vtorkonak@gmail.com>
2021-03-22 14:55:10 +0200

    Lesson 96 image uploading, shouldComponentUpdate fix

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,7 +6,10 @@ import MyPostsContainer from "./MyPosts/MyPostsContainer";
 const Profile = (props) => {
     return (
         <div>
-            <ProfileInfo profile={props.profile} status={props.status}
+            <ProfileInfo savePhoto={props.savePhoto} 
+            isOwner={props.isOwner} 
+            profile={props.profile} 
+            status={props.status}
             updateStatus={props.updateStatus}/>
             <MyPostsContainer />
         </div>
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,17 +2,25 @@ import React from "react";
 import Preloader from "../../common/Preloader/Preloader";
 import s from "./ProfileInfo.module.css";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
+import userPhoto from "../../../assets/images/user.png";
 
-const ProfileInfo = ({profile, status, updateStatus}) => {
+const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
     if (!profile) {
         return <Preloader />
+    }
+
+    const onMainPhotoSelected = (e) => {
+        if (e.target.files.length) {
+            savePhoto(e.target.files[0]);
+        }
     }
 
     return (
         <div>
             <div className={s.descriptionBlock}>
-                <img src={profile.photos.large} />
-                <ProfileStatusWithHooks status={status} updateStatus= {updateStatus}/>
+                <img src={profile.photos.large || userPhoto} className={s.mainPhoto} />
+                {isOwner && <input type={"file"} onChange={onMainPhotoSelected}/>}
+                <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
             </div>
         </div>
     )
